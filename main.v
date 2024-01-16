8a3b206295829dbbd780a34a0e368bd3eed3cea3
Eliyaan <nopana1007@gmail.com>
2024-01-16 16:15:01 +0100

    zoom [m/p] but centered on 0,0 (and not image is not oriented)

--- main.v
+++ main.v
@@ -9,17 +9,121 @@ const tile_size = 10
 const theme     = ggui.CatppuchinMocha{}
 const buttons_shape	= ggui.RoundedShape{20, 20, 5, .top_left}
 const not_image = [
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000)],
-	[u32(0xFF00_0000), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_00FF), u32(0xFF00_0000)],
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{0,0,0,255}Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{0,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{0,0,0,255},
+	Color{0,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{0,0,0,255},
+	Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255},Color{255,0,0,255}
 ]
+
+
+@[inline]
+fn a_coords(y int, x int, size int) int {
+	return y * size + x
+}
+
+@[inline]
+fn ceil(nb f64) int {
+	return -int(-nb)
+}
+
+@[direct_array_access]
+fn scale_img(a []Color, scale_goal f64, x_size int, y_size int) []Color {
+	base_side_x := x_size
+	base_side_y := y_size
+	scaled_side_x := ceil(f64(base_side_x) * scale_goal)
+	scaled_side_y := ceil(f64(base_side_y) * scale_goal)
+	if scaled_side_y != base_side_y && scaled_side_x != base_side_x {
+		mut new_a := []Color{len: scaled_side_y * scaled_side_x}
+		for l in 0 .. scaled_side_y {
+			for c in 0 .. scaled_side_x {
+				// Index in the new array of the current pixel
+				new_i := l * scaled_side_y + c
+				// needs division (for proportionality) but only if needed :
+				mut val_l := f64(l * (base_side_y - 1))
+				mut val_c := f64(c * (base_side_x - 1))
+
+				// if the division is a integer (it corresponds to an exact pixel)
+				l_is_int := int(val_l) % (scaled_side_y - 1) != 0
+				c_is_int := int(val_c) % (scaled_side_x - 1) != 0
+				// divide
+				val_l /= (scaled_side_y - 1)
+				val_c /= (scaled_side_x - 1)
+				int_val_l := int(val_l)
+				int_val_c := int(val_c)
+				// Take the right pixel values
+				if l_is_int && c_is_int {
+					new_a[new_i] = a[int(val_l) * base_side_x + int_val_c]
+				} else if !(l_is_int || c_is_int) {  // none of them
+					new_a[new_i].r = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].r * float_gap(val_c) * float_gap(val_l) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].r * float_offset(val_c) * float_gap(val_l) +
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].r * float_offset(val_l) * float_gap(val_c) +
+						a[a_coords(ceil(val_l), ceil(val_c), base_side_x)].r * float_offset(val_l) * float_offset(val_c))
+					new_a[new_i].g = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].g * float_gap(val_c) * float_gap(val_l) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].g * float_offset(val_c) * float_gap(val_l) +
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].g * float_offset(val_l) * float_gap(val_c) +
+						a[a_coords(ceil(val_l), ceil(val_c), base_side_x)].g * float_offset(val_l) * float_offset(val_c))
+					new_a[new_i].b = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].b * float_gap(val_c) * float_gap(val_l) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].b * float_offset(val_c) * float_gap(val_l) +
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].b * float_offset(val_l) * float_gap(val_c) +
+						a[a_coords(ceil(val_l), ceil(val_c), base_side_x)].b * float_offset(val_l) * float_offset(val_c))
+					new_a[new_i].a = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].a * float_gap(val_c) * float_gap(val_l) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].a * float_offset(val_c) * float_gap(val_l) +
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].a * float_offset(val_l) * float_gap(val_c) +
+						a[a_coords(ceil(val_l), ceil(val_c), base_side_x)].a * float_offset(val_l) * float_offset(val_c))
+				} else if l_is_int {  // exact line (not useful for squares I think but there if needed)
+					new_a[new_i].r = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].r * float_gap(val_c) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].r * float_offset(val_c))
+					new_a[new_i].g = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].g * float_gap(val_c) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].g * float_offset(val_c))
+					new_a[new_i].b = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].b * float_gap(val_c) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].b * float_offset(val_c))
+					new_a[new_i].a = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].a * float_gap(val_c) +
+						a[a_coords(int_val_l, ceil(val_c), base_side_x)].a * float_offset(val_c))
+				} else {  // exact collumn (not useful for squares I think but there if needed)
+					new_a[new_i].r = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].r * float_gap(val_l) + 
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].r * float_offset(val_l))
+					new_a[new_i].g = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].g * float_gap(val_l) + 
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].g * float_offset(val_l))
+					new_a[new_i].b = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].b * float_gap(val_l) + 
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].b * float_offset(val_l))
+					new_a[new_i].a = u8(a[a_coords(int_val_l, int_val_c, base_side_x)].a * float_gap(val_l) + 
+						a[a_coords(ceil(val_l), int_val_c, base_side_x)].a * float_offset(val_l))
+				}
+			}
+		}
+		return new_a // needs to be cropped
+	} else {
+		return a
+	}
+}
+
+@[inline]
+fn float_offset(f f64) f64 {
+	return f - int(f)
+}
+
+@[inline]
+fn float_gap(f f64) f64 {
+	return 1 - float_offset(f)
+}
+
+struct Color {
+mut:
+	r u8
+	g u8
+	b u8
+	a u8
+}
+
+fn (c Color) u32() u32 {
+	return (u32(c.a) << 24) | (u32(c.b) << 16) | (u32(c.g) << 8) | c.r
+}
 
 enum Id {
 	@none
@@ -90,6 +194,8 @@ mut:
 	screen_pixels [768][1366]u32 = [768][1366]u32{init:[1366]u32{init:u32(0xFFBBBBBB)}}
 	viewport_x int
 	viewport_y int
+
+	scale f64 = 2
 }
 
 
@@ -239,12 +345,13 @@ fn on_frame(mut app App) {
     app.gg.begin()
 	// calculate the images at the right scale
 	app.screen_pixels = [768][1366]u32{init:[1366]u32{init:u32(0xFFBBBBBB)}}
+	not_image_scaled := scale_img(not_image, app.scale, tile_size, tile_size)
 	for chunk in app.chunks {
 		for line in chunk.tiles {
 			for id_element in line {
 				if id_element >= 0 {
 					mut element := &app.elements[id_element]
-					if element.x*tile_size+tile_size-1 + app.viewport_x < 1366 && element.y*tile_size+tile_size-1  + app.viewport_y < 768 {
+					if element.x*ceil(tile_size*app.scale)+ceil(tile_size*app.scale)-1 + app.viewport_x < 1366 && element.y*ceil(tile_size*app.scale)+ceil(tile_size*app.scale)-1  + app.viewport_y < 768 {
 						match mut element {
 							Not {
 								rotation := match element.orientation {
@@ -253,17 +360,17 @@ fn on_frame(mut app App) {
 									.east { 0 }
 									.west { 180 }
 								}
-								for y in 0..tile_size {
-									for x in 0..tile_size {
-										app.screen_pixels[element.y*tile_size+y + app.viewport_y][element.x*tile_size+x + app.viewport_x] = not_image[y][x]
+								for y in 0..ceil(tile_size*app.scale) {
+									for x in 0..ceil(tile_size*app.scale) {
+										app.screen_pixels[element.y*ceil(tile_size*app.scale)+y + app.viewport_y][element.x*ceil(tile_size*app.scale)+x + app.viewport_x] = not_image_scaled[y*ceil(tile_size*app.scale)+x].u32()
 									}
 								}
 							}
 							Wire {
 								color := if app.wire_groups[element.id_glob_wire].on() {u32(0xFF12_D0_EF)} else {u32(0xFF00_0000)}
-								for y in 0..tile_size {
-									for x in 0..tile_size {
-										app.screen_pixels[element.y*tile_size+y + app.viewport_y][element.x*tile_size+x + app.viewport_x] = color
+								for y in 0..ceil(tile_size*app.scale) {
+									for x in 0..ceil(tile_size*app.scale) {
+										app.screen_pixels[element.y*ceil(tile_size*app.scale)+y + app.viewport_y][element.x*ceil(tile_size*app.scale)+x + app.viewport_x] = color
 									}
 								}
 							}
@@ -278,18 +385,18 @@ fn on_frame(mut app App) {
 	match app.build_selected_type {
 		.not {
 			color := gg.Color{50, 100, 100, 100}
-			app.gg.draw_square_filled(f32(app.mouse_x*tile_size+app.viewport_x%tile_size), f32(app.mouse_y*tile_size+app.viewport_y%tile_size), tile_size, gg.Color{100, 100, 100, 100})
+			app.gg.draw_square_filled(f32(app.mouse_x*ceil(tile_size*app.scale)+app.viewport_x%ceil(tile_size*app.scale)), f32(app.mouse_y*ceil(tile_size*app.scale)+app.viewport_y%ceil(tile_size*app.scale)), ceil(tile_size*app.scale), gg.Color{100, 100, 100, 100})
 			rotation := match app.build_orientation {
 				.north { -90 }
 				.south { 90 }
 				.east { 0 }
 				.west { 180 }
 			}
-			app.gg.draw_polygon_filled(f32(app.mouse_x*tile_size+app.viewport_x%tile_size) + tile_size/2.0, f32(app.mouse_y*tile_size+app.viewport_y%tile_size) + tile_size/2.0, tile_size/2.0, 3, rotation, color)
+			app.gg.draw_polygon_filled(f32(app.mouse_x*ceil(tile_size*app.scale)+app.viewport_x%ceil(tile_size*app.scale)) + f32(ceil(tile_size*app.scale))/2.0, f32(app.mouse_y*ceil(tile_size*app.scale)+app.viewport_y%ceil(tile_size*app.scale)) + f32(ceil(tile_size*app.scale))/2.0, f32(ceil(tile_size*app.scale))/2.0, 3, rotation, color)
 		}
 		.wire {
 			color := gg.Color{100, 100, 100, 100}
-			app.gg.draw_square_filled(f32(app.mouse_x*tile_size+app.viewport_x%tile_size), f32(app.mouse_y*tile_size+app.viewport_y%tile_size), tile_size, color)
+			app.gg.draw_square_filled(f32(app.mouse_x*ceil(tile_size*app.scale)+app.viewport_x%ceil(tile_size*app.scale)), f32(app.mouse_y*ceil(tile_size*app.scale)+app.viewport_y%ceil(tile_size*app.scale)), ceil(tile_size*app.scale), color)
 		}
 		else {}
 	}
@@ -299,7 +406,7 @@ fn on_frame(mut app App) {
 }
 
 fn on_event(e &gg.Event, mut app App){
-	app.mouse_x, app.mouse_y = mouse_to_coords(e.mouse_x-app.viewport_x%tile_size, e.mouse_y-app.viewport_y%tile_size)
+	app.mouse_x, app.mouse_y = app.mouse_to_coords(e.mouse_x-app.viewport_x%ceil(tile_size*app.scale), e.mouse_y-app.viewport_y%ceil(tile_size*app.scale))
 	app.screen_mouse_x, app.screen_mouse_y = int(e.mouse_x), int(e.mouse_y)
     match e.typ {
         .key_down {
@@ -322,7 +429,9 @@ fn on_event(e &gg.Event, mut app App){
 				.s {app.viewport_y -= 5}
 				.a {app.viewport_x += 5}
 				.d {app.viewport_x -= 5}
-                else {}
+				.semicolon {if app.scale > 0.1 {app.scale -= 0.1} }
+				.p {app.scale += 0.1}
+                else {dump(e.key_code)}
             }
 			if app.debug_mode && (app.build_orientation != orientation_before || app.build_selected_type != type_before){
 				println("app.build_selected_type = .$app.build_selected_type")
@@ -333,10 +442,10 @@ fn on_event(e &gg.Event, mut app App){
 			if !(e.mouse_x < 160 && e.mouse_y < 30) {
 				match e.mouse_button{
 					.left{
-						app.place_in(app.mouse_x - (app.viewport_x)/tile_size, app.mouse_y - (app.viewport_y)/tile_size) or {}
+						app.place_in(app.mouse_x - (app.viewport_x)/ceil(tile_size*app.scale), app.mouse_y - (app.viewport_y)/ceil(tile_size*app.scale)) or {}
 					}
 					.right {
-						app.delete_in(app.mouse_x - (app.viewport_x)/tile_size, app.mouse_y - (app.viewport_y)/tile_size) or {}
+						app.delete_in(app.mouse_x - (app.viewport_x)/ceil(tile_size*app.scale), app.mouse_y - (app.viewport_y)/ceil(tile_size*app.scale)) or {}
 					}
 					else{}
 				}
@@ -365,8 +474,8 @@ fn (mut app App) get_tile_id_at(x int, y int) i64 {
 	return chunk.tiles[math.abs(y-chunk.y*16)][math.abs(x-chunk.x*16)]
 }
 
-fn mouse_to_coords(x f32, y f32) (int, int) {
-	return int(x)/tile_size, int(y)/tile_size
+fn (app App) mouse_to_coords(x f32, y f32) (int, int) {
+	return int(x)/ceil(tile_size*app.scale), int(y)/ceil(tile_size*app.scale)
 }
 
 // returns the relative coordinates of the input of a not gate
